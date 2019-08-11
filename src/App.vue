9bc2ff09a81abef88a0e4ffb571af18de793f27b
Yuko Kanai <48863614+c3drive@users.noreply.github.com>
2019-08-11 23:18:51 +0900

    Revert "Dev"

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,10 +13,11 @@ export default {
 
 <style>
 #app {
-  font-family: 'Vollkorn', 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
