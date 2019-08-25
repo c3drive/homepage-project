f9f343134ccdc650e48d802ae27cd574729612cf
Yuko Kanai <48863614+c3drive@users.noreply.github.com>
2019-08-25 22:55:05 +0900

    Dev (#52)
    * save tmp 20190811
    
    * Top design css-img bug #14
    
    * save tmp 20190817
    
    * save tmp 20190818
    
    * close #34
    
    * close #37
    
    * save tmp 20190824
    
    * skillPage作成、他ブラッシュアップ
    
    * contact先追加
    
    * 相対パスへ
    
    * 相対パスへ
    
    * 相対パスへ
    
    * 相対パスへ
    
    * 画面が真っ白になるためhistory設定を削除
    
    * 画面が真っ白になるためhistory設定を削除
    
    * 画面が真っ白になるためhistory設定を削除
    
    * work, homepageのおかしいところ修正
    
    * work, homepagのおかしいところ修正
    
    * work, homepagのおかしいところ修正
    
    * imgが認識されなくなったので一旦削除
    
    * imgが認識

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/HomePage'
+import About from '@/pages/AboutPage'
+import Work from '@/pages/WorkPage'
+import Skill from '@/pages/SkillPage'
+import Contact from '@/pages/ContactPage'
 
 Vue.use(Router)
 
 export default new Router({
+  // mode: 'history', //１GitHubPagesで画面が出ないためコメントアウトする
   routes: [
+    // {
+    // path: '/',
+    // name: 'HelloWorld',
+    // component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/work',
+      name: 'Work',
+      component: Work
+    },
+    {
+      path: '/skill',
+      name: 'Skill',
+      component: Skill
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
     }
   ]
 })
