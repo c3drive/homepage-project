4f452ae295150ee1b4a943e6d9a6db5d9930c6d2
c3drive <c3drive@gmail.com>
2019-08-26 01:15:39 +0900

    webpack-dev-serverが3系だとdevエラーが出るためバージョンダウン

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -24,17 +24,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-a {
-  color: white;
-  text-decoration: none;
-}
-.section {
-  width: 20%;
-  margin: 10px;
-  border: 1px solid #dee7ec;
-  background-color:rgba(0, 0, 0, 0.3);
-}
-.section:hover {
-  background-color:rgba(255, 255, 255, 0.3);
-}
 </style>
